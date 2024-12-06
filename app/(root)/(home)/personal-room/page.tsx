724fbf0b3fdb747afbf8587a6ad48883ca5e6b6a
arthurkeeng <keengarthurchima@gmail.com>
2024-12-06 13:54:27 +0100

    Including shadcn's input

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -3,7 +3,7 @@ const PersonalRoom = () => {
   return (
     <section className="flex flex-col gap-10 text-white size-full">
     <h1 className="text-3xl font-bold">
-      Personal Room
+      Personal Room will be implemented in future versionss
     </h1>
   </section>
   )
